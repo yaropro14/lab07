d69cdd5d43116901922d805b5a6aa43324c34575
yaropro14 <yaropro14@gmail.com>
2020-04-09 12:29:07 +0300

    new

---
+++ _builds/clang-cxx14/CMakeFiles/3.16.5/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Darwin-19.3.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "19.3.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/Users/yaroslav/Desktop/Proga/Acronis/labs/yaropro14/workspace/projects/lab07/tools/polly/clang-cxx14.cmake")
+
+set(CMAKE_SYSTEM "Darwin-19.3.0")
+set(CMAKE_SYSTEM_NAME "Darwin")
+set(CMAKE_SYSTEM_VERSION "19.3.0")
+set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+
+set(CMAKE_CROSSCOMPILING "FALSE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ demo/main.cpp
@@ -0,0 +1,19 @@
+#include <print.hpp>
+
+#include <cstdlib>
+
+int main(int argc, char* argv[])
+{
+  const char* log_path = std::getenv("LOG_PATH");
+  if (log_path == nullptr)
+  {
+    std::cerr << "undefined environment variable: LOG_PATH" << std::endl;
+  }
+  std::string text;
+  while (std::cin >> text)
+  {
+    std::ofstream out{log_path, std::ios_base::app};
+    print(text, out);
+    out << std::endl;
+  }
+}
